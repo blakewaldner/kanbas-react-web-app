d1f96016004f3f7829920532fb11b8ac7e3f8f1b
waldnerb <waldner.b@northeastern.edu>
2023-11-06 00:52:01 -0500

    a4 branch

--- src/Kanbas/Courses/Modules/ModuleList.js
+++ src/Kanbas/Courses/Modules/ModuleList.js
@@ -1,47 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
-import db from "../../Database";
-import "./Module.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useSelector, useDispatch } from "react-redux";
 import {
-    faPlus
-} from '@fortawesome/free-solid-svg-icons';
-
-
+    addModule,
+    deleteModule,
+    updateModule,
+    setModule,
+} from "./modulesReducer";
 function ModuleList() {
     const { courseId } = useParams();
-    const modules = db.modules;
+    const modules = useSelector((state) => state.modulesReducer.modules);
+    const module = useSelector((state) => state.modulesReducer.module);
+    const dispatch = useDispatch();
     return (
-        <div class="module-list">
-            <button type="button" className="btn btn-danger float-end button-spacing">
-                <FontAwesomeIcon
-                    icon={faPlus}
-                />
-                Module
-            </button>
-            <div className="dropdown float-end button-spacing">
-                <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Publish all
+        <ul className="list-group">
+            <li className="list-group-item">
+                <textarea
+                    class="form-control"
+                    value={module.name}
+                    onChange={(e) =>
+                        dispatch(setModule({ ...module, name: e.target.value }))
+                    } />
+                <br />
+                <textarea
+                    class="form-control"
+                    value={module.description}
+                    onChange={(e) =>
+                        dispatch(setModule({ ...module, description: e.target.value }))
+                    } />
+                <br />
+                <button class="btn btn-success float-end ms-3"
+                    onClick={() => dispatch(addModule({ ...module, course: courseId }))}>
+                    Add
                 </button>
-            </div>
-            <button type="button" className="btn btn-light float-end button-spacing">View Progress</button>
-            <button type="button" className="btn btn-light float-end">Collapse All</button>
-            <br />
-            <br />
-            
-            <ul className="list-group">
-                {
-                    modules
-                        .filter((module) => module.course === courseId)
-                        .map((module, index) => (
-                            <li key={index} className="list-group-item list-group-item-secondary course-spacing">
-                                <h3>{module.name}</h3>
-                                <p>{module.description}</p>
-                            </li>
-                        ))
-                }
-            </ul>
-        </div>
+
+                <button class="btn btn-primary float-end"
+                    onClick={() => dispatch(updateModule(module))}>
+                    Update
+                </button>
+
+
+            </li>
+            <br/>
+            {modules
+                .filter((module) => module.course === courseId)
+                .map((module, index) => (
+                    <li key={index} className="list-group-item">
+                         
+                        <button class="btn btn-success float-end ms-3"
+                            onClick={() => dispatch(setModule(module))}>
+                            Edit
+                        </button>
+                        <button class="btn btn-danger float-end ms-3"
+                            onClick={() => dispatch(deleteModule(module._id))}>
+                            Delete
+                        </button>
+                        <h3>{module.name}</h3>
+                        <p>{module.description}</p>
+                        <p>{module._id}</p>
+                    </li>
+                ))}
+        </ul>
     );
 }
 export default ModuleList;
--- src/Kanbas/Dashboard/index.js
+++ src/Kanbas/Dashboard/index.js
@@ -1,29 +1,61 @@
+import { React, useState } from "react";
 import { Link } from "react-router-dom";
 import db from "../Database";
-function Dashboard() {
-    const courses = db.courses;
-    const colors = ['#0D6EFD', '#6C757D', '#198754', '##DC3545', '##FFC107', '#0DCAF0', '#F8F9FA'];
+
+function Dashboard(
+    { courses, course, setCourse, addNewCourse,
+        deleteCourse, updateCourse }) 
+    {
     return (
         <div className="body" style={{ width: '150%' }}>
             <h1>Dashboard</h1>
             <hr />
             <h2>Published Courses ({courses.length})</h2>
             <hr />
+            <h5>Course</h5>
+            <input value={course.name} className="form-control"
+                onChange={(e) => setCourse({ ...course, name: e.target.value })} />
+            <input value={course.number} className="form-control"
+                onChange={(e) => setCourse({ ...course, number: e.target.value })} />
+            <input value={course.startDate} className="form-control" type="date"
+                onChange={(e) => setCourse({ ...course, startDate: e.target.value })} />
+            <input value={course.endDate} className="form-control" type="date"
+                onChange={(e) => setCourse({ ...course, endDate: e.target.value })} />
 
-            <div className="d-flex flex-row flex-wrap">
-                {courses.map((course, index) => (
-                    <Link key={course._id} to={`../courses/${course._id}`} className="dashboard-card" style={{ textDecoration: 'none', margin: '10px' }}>
-                        <div className="card">
-                            <div className="card-header" style={{ height: '150px', backgroundColor: colors[index % colors.length] }}></div>
-                            <div className="card-body">
-                                <h5 className="card-title">{course.number} {course.name}</h5>
-                                <h6 className="card-title">{course._id}</h6>
-                                <p className="card-text">{course.startDate} to {course.endDate}</p>
-                            </div>
-                        </div>
+            <button class="btn btn-success course-spacing" onClick={addNewCourse} >
+                Add
+            </button>
+            <button class="btn btn-primary course-spacing" onClick={updateCourse} >
+                Update
+            </button>
+            
+
+            <div className="list-group">
+                {courses.map((course) => (
+                    <Link key={course._id}
+                        to={`../Courses/${course._id}`}
+                        className="list-group-item">
+                        {course.name}
+                        <button class="btn btn-danger float-end ms-2"
+                            onClick={(event) => {
+                                event.preventDefault();
+                                deleteCourse(course._id);
+                            }}>
+                            Delete
+                        </button>
+                        <button class="btn btn-warning float-end me-2"
+                            onClick={(event) => {
+                                event.preventDefault();
+                                setCourse(course);
+                            }}>
+                            Edit
+                        </button>
+
+
                     </Link>
                 ))}
             </div>
+
         </div>
     );
 };
