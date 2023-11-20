90cb79af09725b8ba2a01d5e2351bb0a171da379
waldnerb <waldner.b@northeastern.edu>
2023-11-19 22:00:05 -0500

    fixing url in kanbas index

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -10,7 +10,8 @@ import axios from "axios";
 
 function Kanbas() {
   const [courses, setCourses] = useState([]);
-  const URL = "http://localhost:4000/api/courses";
+  const API_BASE = process.env.REACT_APP_API_BASE;
+  const URL = `${API_BASE}/courses`;
   const deleteCourse = async (course_id) => {
     const response = await axios.delete(
       `${URL}/${course_id}`
