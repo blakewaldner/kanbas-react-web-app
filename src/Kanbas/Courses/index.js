62e9946bc14a9afe9b7e871e6866615051ef3d0b
waldnerb <waldner.b@northeastern.edu>
2023-11-19 21:53:42 -0500

    log

--- src/Kanbas/Courses/index.js
+++ src/Kanbas/Courses/index.js
@@ -13,6 +13,7 @@ function Courses() {
   const { courseId } = useParams();
   const API_BASE = process.env.REACT_APP_API_BASE;
   const URL = `${API_BASE}/courses`;
+  console.log(URL)
   const [course, setCourse] = useState({});
   const findCourseById = async (courseId) => {
     const response = await axios.get(
