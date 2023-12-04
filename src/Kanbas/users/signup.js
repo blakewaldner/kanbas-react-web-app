229a55c8549b26ae4aeb404d0e252f5760d20c14
waldnerb <waldner.b@northeastern.edu>
2023-12-04 12:39:03 -0500

    error chjecking

--- src/Kanbas/users/signup.js
+++ src/Kanbas/users/signup.js
@@ -2,42 +2,48 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import * as client from "./client";
 function Signup() {
-  const [error, setError] = useState("");
-  const [credentials, setCredentials] = useState({
-    username: "", password: "" });
-  const navigate = useNavigate();
-  const signup = async () => {
-    try {
-      await client.signup(credentials);
-      navigate("/kanbas/account");
-    } catch (err) {
-      setError(err.response.data.message);
-    }
-  };
-  return (
-    <div>
-        
-      <h1>Signup</h1>
-      <hr/>
-      {error && <div>{error}</div>}
-      <input
-        value={credentials.username}
-        className="form-control my-2"
-        placeholder="Enter Username"
-        onChange={(e) => setCredentials({
-          ...credentials,
-          username: e.target.value })} />
-      <input
-        value={credentials.password}
-        className="form-control my-2"
-        placeholder="Enter Password"
-        onChange={(e) => setCredentials({
-          ...credentials,
-          password: e.target.value })} />
-      <button class="btn btn-primary my-2" onClick={signup}>
-        Signup
-      </button>
-    </div>
-  );
+    const [error, setError] = useState("");
+    const [credentials, setCredentials] = useState({
+        username: "", password: ""
+    });
+    const navigate = useNavigate();
+    const signup = async () => {
+        try {
+            const BASE_API = process.env.REACT_APP_BASE_API_URL;
+            console.log(BASE_API)
+            console.log(credentials)
+            await client.signup(credentials);
+            navigate("/kanbas/account");
+        } catch (err) {
+            setError(err.response.data.message);
+        }
+    };
+    return (
+        <div>
+
+            <h1>Signup</h1>
+            <hr />
+            {error && <div>{error}</div>}
+            <input
+                value={credentials.username}
+                className="form-control my-2"
+                placeholder="Enter Username"
+                onChange={(e) => setCredentials({
+                    ...credentials,
+                    username: e.target.value
+                })} />
+            <input
+                value={credentials.password}
+                className="form-control my-2"
+                placeholder="Enter Password"
+                onChange={(e) => setCredentials({
+                    ...credentials,
+                    password: e.target.value
+                })} />
+            <button class="btn btn-primary my-2" onClick={signup}>
+                Signup
+            </button>
+        </div>
+    );
 }
 export default Signup;
